e4674365d7bbe38245c21ddf5207b2a93826d4d4
José Pedro Cordero <peterotool@gmail.com>
2025-01-04 02:11:27 -0300

    Add submodule stow/nvim/.config/nvim

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 
 Based on the following repos:
 
-* [protiumx](https://github.com/protiumx/.dotfiles)
-* [andrew8088](https://github.com/andrew8088/dotfiles)
-* [ThePrimeagen](https://github.com/ThePrimeagen/.dotfiles)
-* [agilesteel](https://github.com/agilesteel/.dotfiles)
+- [protiumx](https://github.com/protiumx/.dotfiles)
+- [andrew8088](https://github.com/andrew8088/dotfiles)
+- [ThePrimeagen](https://github.com/ThePrimeagen/.dotfiles)
+- [agilesteel](https://github.com/agilesteel/.dotfiles)
+- [omerxx](https://github.com/omerxx/dotfiles)
 
 ## Installing
 
@@ -15,11 +16,21 @@ curl -sO https://raw.githubusercontent.com/peterotool/.dotfiles/main/dotfiles
 
 ## Main Scripts
 
-* scripts/config.sh: general settings and dotfiles
-* scripts/osx.sh: MacOS settings
+- scripts/config.sh: general settings and dotfiles
+- scripts/osx.sh: MacOS settings
 
 ## Testing Stow
 
 ```sh
 stow -nSv vim
+```
+
+## Adding Git Submodules
+
+To add Git submodules, run the following commands:
+
+```sh
+git submodule init
+git submodule update --recursive --remote
+git submodule foreach --recursive git pull origin master
 ```
---
+++ scripts/install-brew-packages.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+brew install --cask alacritty
+brew install --cask bitwarden
+brew install --cask cursor
+brew install --cask dbeaver-community
+brew install --cask font-meslo-lg-nerd-font
+brew install --cask ghostty
+brew install --cask github
+brew install --cask google-chat
+brew install --cask google-chrome
+brew install --cask google-cloud-sdk
+brew install --cask google-drive
+brew install --cask handbrake
+brew install --cask keycastr
+brew install --cask ngrok
+brew install --cask numi
+brew install --cask slack
+brew install --cask visual-studio-code
+brew install --cask zoom
+
+brew install docker
+brew install docker-compose
+
+brew install lua-language-server # https://github.com/LuaLS/lua-language-server
+brew install neovim
+brew install starship
+
+brew install go
+brew install lua
+brew install node
+brew install rust
+brew install terraform
+brew install tmux
+
+brew install ollama
+
+brew install antigen # Plugin manager for Zsh that include Oh My Zsh plugins and custom repositories
+brew install bat
+brew install cheat
+brew install discord
+brew install fd
+brew install ffmpeg # converting video and audio (preview video)
+brew install fzf
+brew install gh
+brew install git-delta # https://github.com/dandavison/delta
+brew install htop
+brew install imagemagick # for SVG, Font, HEIC, and JPEG XL preview
+brew install jq
+brew install jsonpp
+brew install libomp # XGBoost Library
+brew install markdownlint-cli
+brew install mpv     # Media player based on MPlayer and mplayer3
+brew install poppler # pdf rendering library (preview pdf)
+brew install ripgrep
+brew install shellcheck
+brew install stow
+brew install taplo # https://github.com/tamasfe/taplo
+brew install the_silver_searcher
+brew install tldr
+brew install trash
+brew install tree
+brew install wget
+brew install yazi # terminal file manager
+brew install yt-dlp
+brew install zoxide
+brew install zsh-syntax-highlighting
+
+brew install helm
+brew install kubernetes-cli
