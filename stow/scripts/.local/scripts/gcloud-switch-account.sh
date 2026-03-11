8b53b729d688511573d8b2a69c0a65d01caa51bf
José Pedro Cordero <peterotool@gmail.com>
2026-03-10 23:43:51 -0300

    feat: update gcloud-switch-account.sh

--- stow/scripts/.local/scripts/gcloud-switch-account.sh
+++ stow/scripts/.local/scripts/gcloud-switch-account.sh
@@ -3,26 +3,35 @@
 # Get list of authenticated accounts
 accounts=$(gcloud auth list --format="value(account)")
 
-# Check if any accounts exist
 if [ -z "$accounts" ]; then
     echo "No authenticated accounts found. Please run 'gcloud auth login' first."
     exit 1
 fi
 
-# Display accounts and let the user select one
 echo "Available authenticated accounts:"
-PS3="> "
+PS3="Select account > "
 select account in $accounts; do
     if [ -n "$account" ]; then
         echo "Activating account: $account"
         gcloud config set account "$account"
-        echo "Now using account: $(gcloud config get-value account)"
 
-        # Check if this is the special account
         if [ "$account" = "josecordero.imagemaker@latam.com" ]; then
-            echo "Setting project to revenue-management-data-dev..."
-            gcloud config set project revenue-management-data-dev
-            echo "Now using project: $(gcloud config get-value project)"
+            echo "Select environment:"
+            select env in "dev" "prod"; do
+                case $env in
+                dev)
+                    gcloud config set project revenue-management-data-dev
+                    break
+                    ;;
+                prod)
+                    gcloud config set project revenue-management-data-prod
+                    break
+                    ;;
+                *)
+                    echo "Invalid selection."
+                    ;;
+                esac
+            done
         else
             echo "Fetching available projects..."
             projects=$(gcloud projects list --format="value(projectId)")
@@ -35,17 +44,21 @@ select account in $accounts; do
             echo "Available projects:"
             select project in $projects; do
                 if [ -n "$project" ]; then
-                    echo "Setting project to: $project"
                     gcloud config set project "$project"
-                    echo "Now using project: $(gcloud config get-value project)"
                     break
                 else
-                    echo "Invalid selection. Please try again."
+                    echo "Invalid selection."
                 fi
             done
         fi
+
+        echo "-----------------------------------"
+        echo "Now using account: $(gcloud config get-value account)"
+        echo "Now using project: $(gcloud config get-value project)"
+        echo "-----------------------------------"
+
         break
     else
-        echo "Invalid selection. Please try again."
+        echo "Invalid selection."
     fi
 done
